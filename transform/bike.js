d23ad1ccd1d2e01a748c8bb9082edcd5ace63737
Marco Kaufmann <me@marcokaufmann.ch>
2024-10-10 23:24:20 +0200

    feat: add popup to map

--- transform/bike.js
+++ transform/bike.js
@@ -2,8 +2,8 @@ import data from '../data/bike_parking.json' with { type: 'json' }
 import fs from 'fs'
 import path from 'path'
 
-const markers = data.features.map(feature => 
-    feature.geometry.coordinates)
+const markers = data.features.map(feature => {
+    return { name:feature.properties.name, coordinates: feature.geometry.coordinates}})
 
 fs.writeFileSync(path.resolve('./public/markers/bike.json'), JSON.stringify(markers))
 
--- transform/park-rail.js
+++ transform/park-rail.js
@@ -2,7 +2,7 @@ import data from '../data/parking-facilities.json' with { type: 'json' }
 import fs from 'fs'
 import path from 'path'
 
-const markers = data.features.map(feature => 
-    feature.geometry.coordinates)
+const markers = data.features.map(feature => {
+    return { name: 'P+R ' + feature.properties.displayName, coordinates: feature.geometry.coordinates}})
 
 fs.writeFileSync(path.resolve('./public/markers/parking.json'), JSON.stringify(markers))
